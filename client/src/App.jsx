7a428b240a3fe94926b06497c2a706fb277edc1e
Aditya <adityadarade2005@gmail.com>
2025-09-28 15:13:59 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import HeroSection from "./pages/student/HeroSection";
 import MainLayout from "./layout/MainLayout";
 import Courses from "./pages/student/Courses";
