f90bc040883fee6ebbe37ce73853f1372ef0620a
Aditya <adityadarade2005@gmail.com>
2025-10-24 20:45:48 +0530

    final commit

--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -8,13 +8,13 @@ router.route("/").post(isAuthenticated,createCourse);
 router.route("/search").get(isAuthenticated, searchCourse);
 router.route("/published-courses").get( getPublishedCourse);
 router.route("/").get(isAuthenticated,getCreatorCourses);
-router.route("/:courseId").put(isAuthenticated,upload.single("courseThumbnail"),editCourse);
-router.route("/:courseId").get(isAuthenticated, getCourseById);
+router.route("/:courseId/lecture/:lectureId").post(isAuthenticated, editLecture);
 router.route("/:courseId/lecture").post(isAuthenticated, createLecture);
 router.route("/:courseId/lecture").get(isAuthenticated, getCourseLecture);
-router.route("/:courseId/lecture/:lectureId").post(isAuthenticated, editLecture);
 router.route("/lecture/:lectureId").delete(isAuthenticated, removeLecture);
 router.route("/lecture/:lectureId").get(isAuthenticated, getLectureById);
+router.route("/:courseId").put(isAuthenticated,upload.single("courseThumbnail"),editCourse);
+router.route("/:courseId").get(isAuthenticated, getCourseById);
 router.route("/:courseId").patch(isAuthenticated, togglePublishCourse);
 
 
